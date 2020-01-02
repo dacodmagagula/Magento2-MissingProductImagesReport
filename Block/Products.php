2478f6ab569af59bd83eabe57fda2130820531a3
Dacod <clerk.dacod@gmail.com>
2020-01-02 10:34:49 +0200

    first commit

---
+++ Block/Products.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dacod\Missingimagesreport\Block;
+
+
+class Products extends \Magento\Framework\View\Element\Template
+{    
+    protected $_productCollectionFactory;
+        
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,        
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,        
+        array $data = []
+    )
+    {    
+        $this->_productCollectionFactory = $productCollectionFactory;    
+        parent::__construct($context, $data);
+    }
+    
+    public function getProductCollection()
+    {
+        $collection = $this->_productCollectionFactory->create();
+        $collection->addAttributeToSelect('*');
+        $collection->setPageSize(3); // fetching only 3 products
+        return $collection;
+    }
+
+    public function sayHello()
+    {
+        return __('Hello World');
+    }
+}
+?>
